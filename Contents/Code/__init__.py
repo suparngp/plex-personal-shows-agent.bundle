5120f07ad7b32e3fb5aeff42eb9d2728f81f0c28
Suparn Gupta <suparngupta@Suparns-Mac-mini.local>
2019-02-17 21:10:32 -0800

    Save Point

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,5 +1,8 @@
 import os, json
 import urllib
+import sys
+import Framework
+
 
 class JSONAgent(Agent.TV_Shows):
     name = 'Personal Shows'
@@ -8,14 +11,7 @@ class JSONAgent(Agent.TV_Shows):
     persist_stored_files = False
 
     def search(self, results, media, lang):
-        Log('I am here')
-        Log(media.show)
-        Log(media.episode)
-        Log(media.season)
-        Log(media.name)
-        Log(media.filename)
-        
-        
+        Log.Info('Searching Metadata')      
         x = "My Title %s %s %s" % (media.name, media.episode, media.season)
         results.Append(MetadataSearchResult(id = media.filename, score = 100, name=media.filename, lang=Locale.Language.NoLanguage))
 
@@ -29,46 +25,54 @@ class JSONAgent(Agent.TV_Shows):
 
         metadata = json.loads(HTTP.Request(url=('http://%s/library/metadata/%s' %(host, season_id)), immediate=True, headers={'Accept': 'application/json'}).content)
         section_id = metadata['MediaContainer']['librarySectionID']
-        Log('sectionId')
-        Log(section_id)
-        request = HTTP.Request(url=('http://%s/library/sections/%s/all?summary.value=%s&type=3&id=%s' % (host, section_id, urllib.quote(summary), season_id)), method='PUT' )
-        
-        request.load()
-        response = request.content
-        Log(response)
 
+        request = HTTP.Request(url=('http://%s/library/sections/%s/all?summary.value=%s&type=3&id=%s' % (host, section_id, urllib.quote(summary), season_id)), method='PUT' )        
+        request.load()
 
     def update(self, metadata, media, lang):
-        Log('I am in update')
-        Log(media.seasons.keys())
-        Log(dir(media.seasons['1']))
-        Log(media.seasons['1'].title)
+        Log.Info('Updating Metadata')
+        
         main_path = media.seasons['1'].episodes['1'].items[0].parts[0].file
-        Log(main_path)
+
         show_path = os.path.normpath(os.path.join(main_path, '../../'))
         show_name = os.path.basename(show_path)
         metadata.title = show_name
 
+        meta_json = None
         meta_path = os.path.join(show_path, 'meta.json')
-        meta_json = json.loads(Core.storage.load(meta_path))
-        Log(meta_json)
-        metadata.posters[meta_json['show_thumbnail']] = Proxy.Preview(None)
-        metadata.genres.clear()
-        for genre in meta_json['tags']:
-            metadata.genres.add(genre)
-        metadata.studio = meta_json['publisher']
-    
+        if os.path.exists(meta_path):
+            meta_json = json.loads(Core.storage.load(meta_path))
+            Log.Info(meta_json)
+            metadata.posters[meta_json.get('show_thumbnail', '')] = Proxy.Preview(None)
+            metadata.studio = meta_json.get('publisher', '')
+            metadata.genres.clear()
+            for genre in meta_json.get('tags', []):
+                metadata.genres.add(genre)
+
+            metadata.roles.clear()
+            for actor in meta_json.get('actors', []):
+                role = metadata.roles.new()
+                role.role = actor.get('role', '')
+                role.name = actor.get('name', '')
+                role.photo = actor.get('photo', '')
+            
         for season_index in media.seasons.keys():
+            season_metadata = metadata.seasons[season_index]
             episode_keys = media.seasons[season_index].episodes.keys()
             first_episode_path = media.seasons[season_index].episodes[episode_keys[0]].items[0].parts[0].file
-            metadata.seasons[season_index].posters[meta_json['season_thumbnails'][season_index]] = Proxy.Preview(None)
+            if meta_json:
+                season_thumbs = meta_json.get('season_thumbnails')
+                if season_thumbs:
+                    season_metadata.posters[season_thumbs.get(season_index, '')] = Proxy.Preview(None)
+
             season_path = os.path.normpath(os.path.join(first_episode_path, '../'))
-            Log(season_path)
-            metadata.seasons[season_index].summary = os.path.basename(season_path)
+            season_metadata.summary = os.path.basename(season_path)
             self.update_season(media.seasons[season_index].id, os.path.basename(season_path))
+            
             for episode_index in media.seasons[season_index].episodes.keys():
+                episode_metadata = season_metadata.episodes[episode_index]
                 episode_path = media.seasons[season_index].episodes[episode_index].items[0].parts[0].file
                 episode_file_name = os.path.basename(episode_path)
                 filtered_name = os.path.splitext(episode_file_name)[0].replace('S%sE%s - ' % (season_index, episode_index), '')
                 episode_name = '%s - %s' %(str(episode_index).zfill(2), filtered_name)
-                metadata.seasons[season_index].episodes[episode_index].title = episode_name
+                episode_metadata.title = episode_name
